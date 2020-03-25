b5fd15f5645b12e7072cbd8b4338c299d2149a23
Angelo Ibarrola <1105vino@gmail.com>
2020-03-25 14:41:16 -0700

    Index

--- project/app_folder/routes.py
+++
@@ -1,8 +0,0 @@
-from app_folder import app
-from flask import render_template
-
-@app.route('/')
-def home():
-    myname = 'carlos' 
-    mylist = ['carlos', 'jane', 'alex']
-    return render_template('index.html', name=myname, names=mylist)
--- project/run.py
+++
@@ -1,2 +0,0 @@
-from app_folder import app
-app.run()
