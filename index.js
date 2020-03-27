8816262a5627d299fa875b46654d8a5bb44d11bd
Angelo Ibarrola <1105vino@gmail.com>
2020-03-26 20:48:02 -0700

    SECRET CODE FIX

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 
 
-function showImage(){
+// function showImage(){
+//     var secretcode = "123";
+//     var input = document.getElementById("secretcode").value;
+//     var img = document.getElementById("starterpack");
+//     if (input === secretcode){
+//         //img.style.visibility = 'visible';                                 // this quickly open the image and refreshes and the image is gone
+//         //document.body.innerHTML = '<img src = "kuyatestarterpack.png">';  //this makes the site replace everything with the picture
+//         //img.style.display = "block";
+//     }else{
+//         alert("wrong motherfucker");
+//     }
+// }
+
+
+function showHide() {
     var secretcode = "123";
-    var input = document.getElementById("secretcode");
+    var input = document.getElementById("secretcode").value;
     var img = document.getElementById("starterpack");
-
     if (input === secretcode){
-        img.style.visibility = 'visible';
-    }else{
-        alert("wrong motherfucker");
+        if (img.style.display == 'none') {
+            img.style.display = 'block';
+          }
+          else {
+            img.style.display = 'none';
+          }
+        }else{
+            alert("that is wrong motherfucker");
+        } 
     }
-
-
-}
